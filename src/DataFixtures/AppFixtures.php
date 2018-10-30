68ea5b28686aac571ea7126a939b96203aa9434a
Yuriy Filonenko <filonenko.us@gmail.com>
2018-10-30 14:33:28 +0200

    Make User entity. Make Migration. Configure security.yaml

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,9 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Provides fake data and load it to database.
@@ -14,7 +17,20 @@ use Faker\Factory;
  */
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+    
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) 
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
+    {
+        $this->loadUsers($manager);
+        //$this->loadMicroPosts($manager);
+    }
+    
+    public function loadMicroPosts(ObjectManager $manager)
     {
         $faker = Factory::create();
 
@@ -22,13 +38,29 @@ class AppFixtures extends Fixture
             $microPost = new MicroPost();
             $microPost
                 ->setMicroPost($faker->text(150))
-                ->setCreatedAt(new \DateTime())
-                ->setUpdatedAt(new \DateTime())
+                ->setCreatedAt(new DateTime())
+                ->setUpdatedAt(new DateTime())
             ;
             
             $manager->persist($microPost);
         }
 
+        $manager->flush();
+    }
+    
+    public function loadUsers(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+        
+        $user = new User;
+        $user
+            ->setUsername('Tom')
+            ->setFullName('Tom Cat')
+            ->setEmail($faker->email)
+            ->setPassword($this->passwordEncoder->encodePassword($user, 'pass123')
+        );
+        
+        $manager->persist($user);
         $manager->flush();
     }
 }
--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
@@ -18,6 +19,8 @@ class MicroPost
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(min=10, minMessage="Minimal message length - 10 characters")
      */
     private $microPost;
 
